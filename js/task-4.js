ee7dd51e1f370008ca0e7fc8626dc51d70189ac9
Ravehell <andreygensirovskyy77@gmail.com>
2026-09-24 11:22:12 +0300

    hw 7

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form")
+form.addEventListener("submit", handlerSubmit)
+function handlerSubmit(event) {
+    event.preventDefault();
+    const { element: { email, password } } = event.currentTarget
+
+    if (email.value === "" || password.value === "") {
+        return alert('All form fields must be filled in')
+    }
+
+    const formData = new FormData(event.currentTarget)
+    formData.forEach((value, name) => {
+        console.log(`${name}, ${value.trim()}`)
+    })
+    event.currentTarget.reset()
+}
+
 
